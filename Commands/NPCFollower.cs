3655a0243ee6204d33246411ab39eabbc03c9be3
Radosław Woźniak <radoslaw.wozniak2000@gmail.com>
2025-10-05 13:28:44 +0200

    better npc

--- Commands/NPCFollower.cs
+++ Commands/NPCFollower.cs
@@ -1,12 +1,9 @@
 using System;
-using UnityEngine;
+using System.Collections.Generic;
 using MEC;
-using PlayerRoles;
 using CommandSystem;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
-using Exiled.Events;
-using Random = System.Random;
 
 namespace RACommands.Commands
 {
@@ -17,13 +14,11 @@ namespace RACommands.Commands
         public string[] Aliases => new[] { "npcf" };
         public string Description => "Spawns Class-D NPC that follows player.";
 
-        private static Translations Translations => Plugin.Instance.Translation;
-
         public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
         {
             if (arguments.Count < 1)
             {
-                response = "Usage: npc <player>";
+                response = "Usage: npc <player> [pre_name|$postname] [post_name]";
                 return false;
             }
 
@@ -33,19 +28,100 @@ namespace RACommands.Commands
                 response = $"Player {arguments.At(0)} not found.";
                 return false;
             }
-            Random random = new Random();
-            string pre = Translations.NpcPreTitle[random.Next(Translations.NpcPreTitle.Count)];
-            string post = Translations.NpcPostTitle[random.Next(Translations.NpcPostTitle.Count)];
-            Npc npc = Npc.Spawn($"{pre} {player.Nickname} {post}", RoleTypeId.ClassD, true, player.Position);
+            String pre = "";
+            String post = "";
+            if (arguments.Count == 2 || arguments.Count == 3)
+            {
+                if (arguments.At(1).StartsWith("$"))
+                {
+                    post = arguments.At(1).Substring(1);
+                }
+                else
+                {
+                    pre = arguments.At(1);
+                }
+            }
+            if (arguments.Count == 3)
+            {
+                post = arguments.At(2);
+            }
+            Npc npc = Npc.Spawn($"{pre} {player.Nickname} {post}", player.Role, true, player.Position);
             npc.MaxHealth = 1;
             npc.Health = 1;
+            Item coin = Item.Create(ItemType.Coin);
+            if (!Plugin.Instance.Npcs.ContainsKey(player))
+            {
+                Plugin.Instance.Npcs[player] = new List<Npc>();
+            }
+            Plugin.Instance.Npcs[player].Add(npc);
             Timing.CallDelayed(1f, () =>
             {
+                npc.AddItem(coin);
+                npc.CurrentItem = coin;
                 npc.Follow(player);
             });
 
             response = $"NPC Follower spawned near player {player.Nickname}";
             return true;
+        }
+
+        public NpcCommand()
+        {
+            Exiled.Events.Handlers.Player.Dying += OnPlayerDying;
+            Exiled.Events.Handlers.Player.Died += OnPlayerDied;
+        }
+
+        private void OnPlayerDying(Exiled.Events.EventArgs.Player.DyingEventArgs ev)
+        {
+            foreach(KeyValuePair<Player, List<Npc>> entry in Plugin.Instance.Npcs)
+            {
+                if (ev.Player.IsNPC)
+                {
+                    Npc npc = (Npc)ev.Player;
+                    foreach(Npc npc_entry in entry.Value)
+                    {
+                        if (npc == npc_entry)
+                        {
+                            npc.ClearInventory(true);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnPlayerDied(Exiled.Events.EventArgs.Player.DiedEventArgs ev)
+        {
+            foreach(KeyValuePair<Player, List<Npc>> entry in Plugin.Instance.Npcs)
+            {
+                if (ev.Player.IsNPC)
+                {
+                    Npc npc = (Npc)ev.Player;
+                    foreach(Npc npc_entry in entry.Value)
+                    {
+                        if (npc == npc_entry)
+                        {
+                            npc.Destroy();
+                            Plugin.Instance.Npcs.Remove(npc);
+                        }
+                    }
+                }
+                else
+                {
+                    if (Plugin.Instance.Npcs.ContainsKey(ev.Player))
+                    {
+                        foreach(Npc npc_entry in entry.Value)
+                        {
+                            npc_entry.Kill(ev.Player.Nickname, "");
+                        }
+                    }
+                }
+            }
+        }
+
+        public void OnDisable()
+        {
+            Exiled.Events.Handlers.Player.Dying -= OnPlayerDying;
+            Exiled.Events.Handlers.Player.Died -= OnPlayerDied;
         }
     }
 }
--- Plugin.cs
+++ Plugin.cs
@@ -1,22 +1,27 @@
 using System;
+using System.Collections.Generic;
 using Exiled.API.Features;
 
 namespace RACommands {
-    public class Plugin : Plugin<Config, Translations>
+    public class Plugin : Plugin<Config>
     {
         public override string Name => "RACommands";
         public override string Author => "KedArch";
         public override Version Version => new Version(1, 0, 0);
 
         public static Plugin Instance;
-
+        private RACommands.Commands.NpcCommand npcCommand;
+        public Dictionary<Player, List<Npc>> Npcs { get; private set; }
         public override void OnEnabled()
         {
           Instance = this;
+          Npcs = new Dictionary<Player, List<Npc>>();
+          npcCommand = new RACommands.Commands.NpcCommand();
         }
 
         public override void OnDisabled()
         {
+          npcCommand.OnDisable();
           Instance = null;
         }
     }
